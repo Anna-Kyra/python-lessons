66d5e0e9820f8474414c07a4c748e2c89ae7b2ca
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-08-16 15:54:57 +0200

    Dictionaries
    Een program maken om afkortingen volledig uit te kunnen schrijven.
    
    monthConvertions = {
        "Jan": "January",
        "Feb": "February",
        "Mar": "March"
    }
    
    > Alle keys (binnen curly brackets) moeten origineel zijn, beetje ID's maar geen classes
    
    monthConvertions = {
        "Jan": "January",
        "Feb": "February",
        "Mar": "March"
    }
    
    print(monthConvertions["Jan"])
    > Als je de key print krijg je de volle naam te ien
    
    monthConvertions = {
        "Jan": "January",
        "Feb": "February",
        "Mar": "March"
    }
    
    print(monthConvertions.get("adg", "Not a valid key"))
    > Je kan ook de .get function gebruiken. Als een key invalid is kan je daarnaast een devault value geven

--- python1/app.py
+++ python1/app.py
@@ -1,14 +1,7 @@
-number1 = float(input("Enter first number: ")) # Met float erachter kan je het antwoord meteen naar een float veranderen
-operator = input("Enter operator: ")
-number2 = float(input("Enter second number: "))
+monthConvertions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March"
+}
 
-if operator == "+":
-    print(number1 + number2)
-elif operator == "-":
-    print(number1 - number2)
-elif operator == "/":
-    print(number1 / number2)
-elif operator == "*":
-    print(number1 * number2)
-else:
-    print("Invalid operator") # Basically de error
+print(monthConvertions.get("adg", "Not a valid key"))
