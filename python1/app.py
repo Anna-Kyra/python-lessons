69de0a715e0373a6849fff769684c9dc081f4f73
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-08-16 16:24:33 +0200

    Building a guessing game
    secret_word = "giraffe"
    guess = ""
    guess_count = 0
    guess_limit = 3
    out_of_guesses = False
    
    while guess != secret_word and not(out_of_guesses): # Als de user die secret word niet heeft geraden gaat het nog een keer door de function
        if guess_count < guess_limit:
            guess = input("Enter guess: ")
            guess_count += 1
        else:
            out_of_guesses = True
    
    if out_of_guesses: #Je hoeft geen === True op te schrijven, hij is standaard true
        print("You lose!!")
    else:
        print("You win!")

--- python1/app.py
+++ python1/app.py
@@ -1,7 +1,17 @@
-i = 1
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-while i <= 10:
-    print(i)
-    i += 1
+while guess != secret_word and not(out_of_guesses): # Als de user die secret word niet heeft geraden gaat het nog een keer door de function
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("Done with loop")
+if out_of_guesses: #Je hoeft geen === True op te schrijven, hij is standaard true
+    print("You lose!!")
+else:
+    print("You win!")
