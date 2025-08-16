94c4bd7ed1488949e939be84447f00695e7ce198
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-08-16 16:34:57 +0200

    for loops
    for letter in "Giraffe Academy":
        print(letter)
    > Loops door de letters
    
    friends = ["Jim", "Karen", "Kevin"]
    for friend in friends:
        print(friend)
    > For loop met een array
    
    for index in range(10):
        print(index)
    > print range van 0 tot 9
    
    for index in range(3, 10):
        print(index)
    > Kan ook rang van 3 tot 9 doen bijvoorbeeld
    
    friends = ["Jim", "Karen", "Kevin"]
    
    for index in range(len(friends)):
        print(index)
    > De lenght function is geschreven als len()
    
    friends = ["Jim", "Karen", "Kevin"]
    
    for index in range(2):
        print(friends[index])
    > Zo kan je de range van de array doen
    
    for index in range(10):
        if index == 0:
            print("first")
        else:
            print("not first")
    > Een if statement binnen een for loop

--- python1/app.py
+++ python1/app.py
@@ -1,17 +1,9 @@
-secret_word = "giraffe"
-guess = ""
-guess_count = 0
-guess_limit = 3
-out_of_guesses = False
 
-while guess != secret_word and not(out_of_guesses): # Als de user die secret word niet heeft geraden gaat het nog een keer door de function
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count += 1
+
+friends = ["Jim", "Karen", "Kevin"]
+
+for index in range(10):
+    if index == 0:
+        print("first")
     else:
-        out_of_guesses = True
-
-if out_of_guesses: #Je hoeft geen === True op te schrijven, hij is standaard true
-    print("You lose!!")
-else:
-    print("You win!")
+        print("not first")
